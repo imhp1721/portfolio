606011de6ab1af478381cde8890b66b611c3826c
Inger-Margrethe <imhp@hotmail.dk>
2024-09-17 11:30:29 +0200

    footer done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,67 @@
+/* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
+
+function Footer() {
+  //Mailto og callto funktionalitet er fra https://www.30secondsofcode.org/react/s/mailto-callto/
+  const Mailto = ({ email, subject = "", body = "", children }) => {
+    let params = subject || body ? "?" : "";
+    if (subject) params += `subject=${encodeURIComponent(subject)}`;
+    if (body) params += `${subject ? "&" : ""}body=${encodeURIComponent(body)}`;
+
+    return <a href={`mailto:${email}${params}`}>{children}</a>;
+  };
+  const Callto = ({ phone, children }) => {
+    return <a href={`tel:${phone}`}>{children}</a>;
+  };
+  return (
+    <footer>
+      <img
+        className="wave"
+        src="src\assets\waves\footer.svg"
+        alt="Wavy footer"
+      />
+      <Link to="/home" className="logo">
+        <img src="src\assets\img\logo.png" alt="im-press logo" />
+        <h4>im-press</h4>
+      </Link>
+      <section className="info">
+        <h2>Inger-Margrethe Holt Povlsen</h2>
+        <div className="so_me">
+          <Link
+            to={
+              "https://www.linkedin.com/in/inger-margrethe-holt-povlsen-85170a28a/"
+            }
+            target="blank"
+          >
+            <img src="src\assets\icons\linkedin.png" alt="LinkedIn logo" />
+          </Link>
+          <Link to={"https://github.com/imhp1721"} target="blank">
+            <img src="src\assets\icons\github.png" alt="GitHub logo" />
+          </Link>
+          <Link to={"https://www.instagram.com/im_holt/"} target="blank">
+            <img src="src\assets\icons\ig.png" alt="Instagram logo" />
+          </Link>
+          <Link to={"https://www.facebook.com/imhp1721"} target="blank">
+            <img src="src\assets\icons\fb.png" alt="Facebook logo" />
+          </Link>
+        </div>
+        <div className="link">
+          <Mailto email="imhp@hotmail.dk"
+            subject="Praktikmulighed hos *tilføj din virksomhed*"
+            body=""
+          >
+            imhp@hotmail.dk
+          </Mailto>
+        </div>
+        <div className="link">
+          <Callto phone="+4551953055">+4551953055</Callto>
+        </div>
+      </section>
+      <section className="copy">
+        <p>Copyright &copy; 2024 im-press | All rights reserved</p>
+      </section>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,43 +1,113 @@
 import { Link } from "react-router-dom";
 import MarqueeFeature from "../components/MarqueeFeature";
+import { RandomBlob } from "react-random-shapes";
+import Footer from "../components/Footer";
 
 //FORSIDE
 
 function Home() {
   return (
-    <main className="home">
-      <section className="info">
-        <article>
-          <h2>Frontend overskrift</h2>
-          <p>
-            Som medarbejder er jeg meget optaget af, at mit arbejde er i orden
-            og af høj kvalitet. Som person er jeg meget omhyggelig, og jeg
-            værdsætter orden, stabilitet og fastlagte processer. Da jeg er meget
-            struktureret og pragmatisk anlagt, holder jeg af, at der er tid til
-            at kunne organisere de opgaver, der skal løses. Det er sådan, jeg
-            sikrer mig, at ambitionsniveauet holdes oppe. Jeg tager stort
-            ejerskab for mine opgaver, og jeg trives bedst med klare visioner og
-            konkrete målsætninger – på den måde undgår man ”slinger i valsen”.
-            Derudover motiveres jeg ofte af, at der er fokus på kvalitet fremfor
-            kvantitet, og jeg bruger gerne tid på at dygtiggøre mig.
-          </p>
-          <p className="cta_txt">Jeg søger praktikplads fra 6. januar 2025.</p>
-          <div>
-            <button className="cta_btn">
-              <Link to={"/kontakt"}>Kontakt mig</Link>
-            </button>
-            <button className="cta_btn">
-              <Link to={"/projekter"}>Se mine projekter</Link>
-            </button>
-          </div>
-        </article>
-        <img src="src\assets\img\portrait.png" alt="Portrætfoto" />
-      </section>
-      <section className="feature">
-        <img src="src\assets\waves\feature_bg.svg" alt="Wave" />
-        <MarqueeFeature />
-      </section>
-    </main>
+    <>
+      <main className="home">
+        <section className="info">
+          <article>
+            <h2>Frontend overskrift</h2>
+            <p>
+              Som medarbejder er jeg meget optaget af, at mit arbejde er i orden
+              og af høj kvalitet. Som person er jeg meget omhyggelig, og jeg
+              værdsætter orden, stabilitet og fastlagte processer. Da jeg er
+              meget struktureret og pragmatisk anlagt, holder jeg af, at der er
+              tid til at kunne organisere de opgaver, der skal løses. Det er
+              sådan, jeg sikrer mig, at ambitionsniveauet holdes oppe. Jeg tager
+              stort ejerskab for mine opgaver, og jeg trives bedst med klare
+              visioner og konkrete målsætninger – på den måde undgår man
+              ”slinger i valsen”. Derudover motiveres jeg ofte af, at der er
+              fokus på kvalitet fremfor kvantitet, og jeg bruger gerne tid på at
+              dygtiggøre mig.
+            </p>
+            <p className="cta_txt">
+              Jeg søger praktikplads fra 6. januar 2025.
+            </p>
+            <div>
+              <button className="cta_btn">
+                <Link to={"/kontakt"}>Kontakt mig</Link>
+              </button>
+              <button className="cta_btn">
+                <Link to={"/projekter"}>Se mine projekter</Link>
+              </button>
+            </div>
+          </article>
+          <img src="src\assets\img\portrait.png" alt="Portrætfoto" />
+        </section>
+        <section className="feature">
+          <img src="src\assets\waves\feature.svg" alt="Wave" />
+          <MarqueeFeature />
+        </section>
+        <section className="skills">
+          <h2>Multimediedesigner i evig udvikling</h2>
+          <article className="frontend">
+            <div className="skill">
+              <p>html</p>
+              <div className="bar">
+                <div className="amount" id="html"></div>
+              </div>
+            </div>
+            <div className="skill">
+              <p>css</p>
+              <div className="bar">
+                <div className="amount" id="css"></div>
+              </div>
+            </div>
+            <div className="skill">
+              <p>react</p>
+              <div className="bar">
+                <div className="amount" id="react"></div>
+              </div>
+            </div>
+            <div className="skill">
+              <p>javascript</p>
+              <div className="bar">
+                <div className="amount" id="javascript"></div>
+              </div>
+            </div>
+          </article>
+          <article className="quote">
+            <p>
+              Som det videbegærlige menneske, jeg er, vil jeg altid søge at
+              tilegne mig ny viden og udvide mine faglige kompetencer.
+            </p>
+            <RandomBlob size={400} className="blob" />
+          </article>
+          <article className="design">
+            <div className="skill">
+              <p>figma</p>
+              <div className="bar">
+                <div className="amount" id="figma"></div>
+              </div>
+            </div>
+            <div className="skill">
+              <p>photoshop</p>
+              <div className="bar">
+                <div className="amount" id="photoshop"></div>
+              </div>
+            </div>
+            <div className="skill">
+              <p>illustrator</p>
+              <div className="bar">
+                <div className="amount" id="illustrator"></div>
+              </div>
+            </div>
+            <div className="skill">
+              <p>adobe express</p>
+              <div className="bar">
+                <div className="amount" id="express"></div>
+              </div>
+            </div>
+          </article>
+        </section>
+      </main>
+      <Footer />
+    </>
   );
 }
 
