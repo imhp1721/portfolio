fdc00009b1d85afd351ca8fa924fad08c523bb23
Inger-Margrethe <imhp@hotmail.dk>
2024-10-16 21:19:47 +0200

    home page updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,10 +28,12 @@ function Home() {
               frontend-specialisering er jeg ikke blot teknisk, men også
               æstetisk orienteret. Jeg stræber efter at skabe designs, der både
               er intuitive og appellerende for brugeren, samtidig med at de er
-              velfungerende og strukturerede i koden. Jeg har sans for at skabe
-              content, hvilket jeg praktiserer gennem mit studiejob, men min
-              sande passion ligger i at skabe harmoni mellem de tekniske og
-              visuelle aspekter af mit fag.
+              velfungerende og strukturerede i koden.
+            </p>
+            <p>
+              Jeg har sans for at skabe content, hvilket jeg praktiserer gennem
+              mit studiejob, men min sande passion ligger i at skabe harmoni
+              mellem de tekniske og visuelle aspekter af mit fag.
             </p>
             <p className="cta_txt">
               Jeg søger praktikplads fra 6. januar 2025.
